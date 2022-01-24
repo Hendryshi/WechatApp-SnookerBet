25a47f5aea9fe05de787fe543a6c5d713e8fdc1f
hendryshi <yejia.shi@hotmail.com>
2022-01-24 17:01:27 +0800

    Add buttom vant tabbar

---
+++ custom-tab-bar/index.js
@@ -0,0 +1,39 @@
+Component({
+  data: {
+    active: 0,
+    list: [
+      {
+        icon: "home-o",
+        text: "首页",
+        url: "/pages/home/home",
+      },
+      {
+        icon: "tv-o",
+        text: "直播",
+        url: "/pages/live/live",
+      },
+      {
+        icon: "user-o",
+        text: "我的",
+        url: "/pages/user/user",
+      },
+    ],
+  },
+  methods: {
+    onChange(event) {
+      // this.setData({ active: event.detail });
+      let _this = this;
+      wx.switchTab({
+        url: _this.data.list[event.detail].url,
+      });
+    },
+    init() {
+      const page = getCurrentPages().pop();
+      this.setData({
+        active: this.data.list.findIndex(
+          (item) => item.url === `/${page.route}`
+        ),
+      });
+    },
+  },
+});
