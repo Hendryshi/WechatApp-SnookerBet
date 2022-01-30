d75ee66f0a75764fded3ed8dd1232280569edaa6
hendryshi <yejia.shi@hotmail.com>
2022-01-30 19:11:19 +0800

    Add predict page

--- pages/predict/predict.js
+++ pages/predict/predict.js
@@ -1,1 +1,27 @@
-Page({})
+Page({
+    data:{
+        show:false,
+        columns:[1,2,3,4,5,6,7,8,9,10],
+        score1:"-"
+    },
+
+    showPopup(e){
+        this.setData({
+            show:true
+        })
+    },
+
+    onClose(){
+        this.setData({
+            show:false
+        })
+    },
+
+    onConfirm(e){
+        console.log("enter" + e)
+        this.setData({
+            score1:e.detail.value,
+            show:false
+        })
+    }
+})
