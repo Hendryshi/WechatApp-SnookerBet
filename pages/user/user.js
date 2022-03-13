d12311728edf8f10cda7bd803eaac8b182dd901b
YEJIA SHI <yejia.shi@hotmail.com>
2022-03-13 20:14:44 +0100

    dice component

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,6 +1,31 @@
 Page({
+  data: {
+    url:'../../images/test/',
+    i:0
+  },
+
+
   onShow() {
     this.getTabBar().init();
+    // this.timers(0)
+  },
+
+  changePoint(){
+    this.timers(0)
+  },
+
+  timers: function(count){
+    var that = this;
+    var j = 1;
+    that.data.timer = setInterval(function(){
+      count ++;
+      j = j%6;
+      j++;
+      that.setData({
+        i:j
+      })
+    }, 20)
+    
   },
 
   sendMsg: function(e) {
